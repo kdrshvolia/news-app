aa1d8f8e3b0a35ec35adde9e5a94970306038c68
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-02 15:50:41 +0300

    Adds NewsSourcesBoard component

--- src/components/NewsSourcesBoard/NewsSourcesBoard.js
+++ src/components/NewsSourcesBoard/NewsSourcesBoard.js
@@ -1,1 +1,29 @@
-import React from 'react';import PropTypes from 'prop-types';const NewsSourcesBoard = ({ sources }) => {  return (    <section>      {sources.map((source) => (        <SourceItem source={source} />      ))}    </section>  );};export default NewsSourcesBoard;
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { loadSources } from '../../redux/actions/sourcesActions';
+
+const NewsSourcesBoard = ({ sources, actions }) => {
+  return <section>{console.log(sources)}</section>;
+};
+
+const mapStateToProps = (state) => {
+  return {
+    sources: state.sources,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: {
+      loadSources: bindActionCreators(loadSources, dispatch),
+    },
+  };
+};
+
+NewsSourcesBoard.propTypes = {
+  sources: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewsSourcesBoard);
