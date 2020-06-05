01b15ed7af99f66e4860b6962b8b15fd0e5b9d98
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-05 14:55:51 +0300

    Adds rendering of Filter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import NewsSourcesBoard from './components/NewsSourcesBoard/NewsSourcesBoard';
+import Filters from './components/Filters/Filters';
 
 function App() {
   return (
     <div className="App">
+      <Filters />
       <NewsSourcesBoard />
     </div>
   );
