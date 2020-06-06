1aed3fceed974b58a7cc9c748d27fa7e9051043d
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-06 20:17:50 +0300

    Adds support of redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,8 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 }
