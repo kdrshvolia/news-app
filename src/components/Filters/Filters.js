fd65c6e55be45462ce1c525dd805a34fb1b250cb
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-07 13:50:14 +0300

    Renders list of selected filters

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import FilterSelect from '../FilterSelect/FilterSelect';
+import SelectedFiltersList from '../SelectedFiltersList/SelectedFiltersList';
 
-const Filters = ({ newsFilters }) => {
+const Filters = ({ newsFilters, selectedFilters }) => {
   return (
     <div>
       {Object.keys(newsFilters).map((key, index) => (
         <FilterSelect type={key} filterParam={newsFilters[key]} />
       ))}
+      {Object.keys(selectedFilters).length === 0 ? null : (
+        <SelectedFiltersList selectedFilters={selectedFilters} />
+      )}
     </div>
   );
 };
@@ -16,6 +20,7 @@ const Filters = ({ newsFilters }) => {
 const mapStateToProps = (state) => {
   return {
     newsFilters: state.newsFilters,
+    selectedFilters: state.filterReducer,
   };
 };
 
