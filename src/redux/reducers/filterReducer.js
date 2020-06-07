7328161a000a067615a52b240d64feb9120d3b53
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-07 16:52:49 +0300

    Adds multiple filters

--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -3,7 +3,12 @@ import * as types from '../actions/actionTypes';
 const filterReducer = (selectedFilters = {}, action) => {
   switch (action.type) {
     case types.SET_FILTER:
-      return { ...selectedFilters, ...action.payload };
+      return {
+        ...selectedFilters,
+        [action.payload.type]: selectedFilters[action.payload.type]
+          ? [...selectedFilters[action.payload.type], action.payload.value]
+          : [action.payload.value],
+      };
     default:
       return selectedFilters;
   }
