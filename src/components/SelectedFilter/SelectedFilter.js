58230e57913ddc45ba7978577d689117c6a4a0d2
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-12 17:29:23 +0300

    Adds styles for SelectedFilter

--- src/components/SelectedFilter/SelectedFilter.js
+++ src/components/SelectedFilter/SelectedFilter.js
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { resetFilter } from '../../redux/actions/filterActions';
+import IconDelete from '../../images/icon-delete.svg';
+import { StyledSelectedFilter } from './StyledComponents';
 
 const SelectedFilter = ({ type, filter, resetFilter }) => {
-  return <div onClick={() => resetFilter(type, filter)}>{filter}</div>;
+  return (
+    <StyledSelectedFilter onClick={() => resetFilter(type, filter)}>
+      {filter}
+      <img src={IconDelete} />
+    </StyledSelectedFilter>
+  );
 };
 
 SelectedFilter.propTypes = {
---
+++ src/components/SelectedFilter/StyledComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledSelectedFilter = styled.div`
+  width: 100px;
+  box-sizing: border-box;
+  padding: 10px;
+  border: 1px solid #000;
+  cursor: pointer;
+`;
+
+export { StyledSelectedFilter };
