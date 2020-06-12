46692be15a60bd2f670c824528f505e5f113378d
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-12 16:47:42 +0300

    Adds styles for SourceItem

--- src/components/SourceItem/SourceItem.js
+++ src/components/SourceItem/SourceItem.js
@@ -1,1 +1,20 @@
-import React from 'react';import PropTypes from 'prop-types';const SourceItem = ({ source }) => {  return <div>{source.name}</div>;};SourceItem.propTypes = {  source: PropTypes.object.isRequired,};export default SourceItem;
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SourceInfo, Source } from './StyledComponents';
+
+const SourceItem = ({ source }) => {
+  return (
+    <Source>
+      {source.name}
+      <SourceInfo>
+        {source.description}
+        {source.url}
+      </SourceInfo>
+    </Source>
+  );
+};
+
+SourceItem.propTypes = {
+  source: PropTypes.object.isRequired,
+};
+export default SourceItem;
---
+++ src/components/SourceItem/StyledComponents.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const SourceInfo = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: -100%;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(147, 204, 230, 0.5);
+  opacity: 0;
+  height: 0;
+`;
+
+const Source = styled.div`
+  position: relative;
+  cursor: pointer;
+  overflow: hidden;
+  max-width: 500px;
+  border: 2px solid #008bc8;
+  margin: 20px 0;
+  padding: 25px 0;
+  &:hover ${SourceInfo} {
+    top: 0;
+    -webkit-transition: all 1s, -webkit-transform 1s;
+    transition: all 1s, transform 1s;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+  }
+`;
+
+export { SourceInfo, Source };
