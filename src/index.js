7f7eb9a6b6a2bf4ea7c5cca4b2399dd442f967fe
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-06-12 20:29:37 +0300

    Wraps App in Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './redux/store';
@@ -11,7 +12,9 @@ const store = configureStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
